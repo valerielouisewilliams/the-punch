f26c238b37b694d1e2519d6a79d4eae2eb0fb522
Valerie Williams <72105440+valerielouisewilliams@users.noreply.github.com>
2025-09-30 10:10:15 -0500

    Add files via upload

---
+++ ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  The Punch
+//
+//  Created by Valerie Williams on 9/30/25.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ The_PunchApp.swift
@@ -0,0 +1,17 @@
+//
+//  The_PunchApp.swift
+//  The Punch
+//
+//  Created by Valerie Williams on 9/30/25.
+//
+
+import SwiftUI
+
+@main
+struct The_PunchApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
