9a504973383846c86b24b3e2e22c1d2890b9c2f3
valerielouisewilliams <valerielouisew@gmail.com>
2026-02-21 14:35:36 -0600

    fixed notificationInboxRoutes.js

--- backend/src/routes/notificationInboxRoutes.js
+++ backend/src/routes/notificationInboxRoutes.js
@@ -1,16 +1,9 @@
 const express = require("express");
 const router = express.Router();
+
 const { authenticateToken } = require("../middleware/authMiddleware");
-const {
-  listInbox,
-  unreadCount,
-  markRead,
-  markAllRead
-} = require("../controllers/notificationInboxController");
+const { listInbox } = require("../controllers/notificationInboxController");
 
 router.get("/", authenticateToken, listInbox);
-router.get("/unread-count", authenticateToken, unreadCount);
-router.patch("/read-all", authenticateToken, markAllRead);
-router.patch("/:id/read", authenticateToken, markRead);
 
 module.exports = router;
